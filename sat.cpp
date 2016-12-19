b46b8ef8c817ac63f613e80be2c5c8c423f95f4c
susu <u1240976@gmail.com>
2016-12-19 21:55:28 +0800

    refactor: add print_sat_solution()

--- sat.cpp
+++ sat.cpp
@@ -2,18 +2,21 @@
 #include <fstream>
 #include <vector>
 #include <deque>
+#include <cstdio>
 
 #include "parser.h"
 #include "utils.h"
 #include "sat_solver.h"
 
 void print_clauses(std::vector<Clause> clauses);
+void print_sat_solution(std::ostream& output_stream, std::vector<BoolVal>& answer);
 
 int main(int argc, char *argv[]){
     
     if( argc != 2 ){
-        std::cerr << "error input parameter" << std::endl;
+        std::cerr << "invalid number of parameter." << std::endl;
         std::cerr << "./yasat [input.cnf]" << std::endl;
+        std::exit(1);
     }
     std::string input_name = argv[1];
     std::string output_name = input_name.substr(0, input_name.size()-4);
@@ -31,40 +34,54 @@ int main(int argc, char *argv[]){
     parse_DIMACS_CNF(input_clauses, max_var_index, input_name.c_str());
 
     std::vector<Clause> clauses(input_clauses);
+#ifdef DEBUG
     // print_clauses(clauses);
+#endif
 
     SatSolver solver;
     solver.set_clauses(clauses, max_var_index);
+    // Solve SAT problem
     bool is_sat = solver.solve();
 
     if( is_sat ){
         output_stream << "s SATISFIABLE" << std::endl;
-        output_stream << "v ";
+
         std::vector<BoolVal> answer = solver.answer();
-        for( int i = 0; i < answer.size(); i++ ){
-            if( answer[i] == BoolVal::TRUE ){
-                output_stream << i + 1 << " ";
-            }
-            else if( answer[i] == BoolVal::FALSE ){
-                output_stream << "-" << i + 1 << " ";
-            }
-            else{
-                // not assigned literal: [don't care condition or error?]
-#ifdef DEBUG
-                output_stream << "@" << i + 1 << " ";
-#else
-                output_stream << i + 1 << " ";
-#endif
-            }
-            // output_stream << i + 1 << ": " << answer[i] << std::endl;
-        }
-        output_stream << "0" << std::endl;
+        print_sat_solution(output_stream, answer);
     }
     else{
         output_stream << "s UNSATISFIABLE" << std::endl;
     }
 
     return 0;
+}
+
+void print_sat_solution(std::ostream& output_stream, std::vector<BoolVal>& answer){
+    /* print answer of SAT solution.
+     *
+     *   the format of (x1=0, x2=1, x3=0) is
+     *   v -1 2 -3 0
+     */
+
+    output_stream << "v ";
+
+    for( int i = 0; i < answer.size(); i++ ){
+        if( answer[i] == BoolVal::TRUE ){
+            output_stream << i + 1 << " ";
+        }
+        else if( answer[i] == BoolVal::FALSE ){
+            output_stream << "-" << i + 1 << " ";
+        }
+        else{
+            // not assigned literal: [don't care condition or error?]
+#ifdef DEBUG
+            output_stream << "@" << i + 1 << " ";
+#else
+            output_stream << i + 1 << " ";
+#endif
+        }
+    }
+    output_stream << "0" << std::endl;
 }
 
 void print_clauses(std::vector<Clause> clauses){
